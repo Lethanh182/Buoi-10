fbebb501d927bde82bfaa73ace0edc4f80cfb5c9
Lethanh182 <thanhtl181224@gmail.com>
2024-07-04 19:32:52 +0700

    Add files via upload

---
+++ Q4.5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+  char s[101];
+  char output[101] = "";
+  int i, j = 0;
+  
+  printf("Nhap vao mot chuoi s  = ");
+  fgets(s, sizeof(s), stdin);
+  s[strcspn(s, "\n")] = '\0';
+  for (i = 0; s[i] != '\0'; i++) {
+    if (isalpha(s[i]) || isspace(s[i])) {
+      output[j] = s[i];
+      j++;
+    }
+  }
+  output[j] = '\0'; 
+  printf("OUTPUT:\n%s\n", output);
+
+  return 0;
+}
